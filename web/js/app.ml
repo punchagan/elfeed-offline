d38d6b9b2c06a8009f783e06be0966f7fad64a18
Puneeth Chaganti <punchagan@muse-amuse.in>
2025-11-03 13:24:24 +0530

    Rename some functions and variables in app.ml

--- web/js/app.ml
+++ web/js/app.ml
@@ -10,14 +10,14 @@ let get_element_by_id_exn id =
   | _ ->
       "Failed to find element with id: " ^ id |> failwith
 
-let setStatus txt =
-  let statusEl = get_element_by_id_exn "status" in
+let set_status txt =
+  let status_el = get_element_by_id_exn "status" in
   let text_node = El.txt (Jstr.v txt) in
-  El.set_children statusEl [text_node]
+  El.set_children status_el [text_node]
 
 let get_query () =
-  let qEl = get_element_by_id_exn "q" in
-  El.prop El.Prop.value qEl |> Jstr.trim
+  let q_el = get_element_by_id_exn "q" in
+  El.prop El.Prop.value q_el |> Jstr.trim
 
 let format_date (data : Jv.t) =
   let ms = Jv.get data "date" in
@@ -27,23 +27,23 @@ let format_date (data : Jv.t) =
   let day = Jv.to_int (Jv.call d "getDate" [||]) in
   Printf.sprintf "%04d-%02d-%02d" year month day |> Jstr.of_string
 
-let makeEntry data =
+let make_entry data =
   let title = Jv.get data "title" |> Jv.to_jstr in
-  let titleEl =
+  let title_el =
     El.v
       ~at:[At.v At.Name.class' (Jstr.of_string "title")]
       (Jstr.of_string "span")
       [El.txt title]
   in
   let feed = Jv.get data "feed" |> (fun x -> Jv.get x "title") |> Jv.to_jstr in
-  let feedEl =
+  let feed_el =
     El.v
       ~at:[At.v At.Name.class' (Jstr.of_string "feed")]
       (Jstr.of_string "span")
       [El.txt feed]
   in
   let date = format_date data in
-  let dateEl =
+  let date_el =
     El.v
       ~at:[At.v At.Name.class' (Jstr.of_string "date")]
       (Jstr.of_string "span")
@@ -52,18 +52,19 @@ let makeEntry data =
   let entry =
     El.v
       ~at:[At.v At.Name.class' (Jstr.of_string "entry")]
-      (Jstr.of_string "div") [dateEl; titleEl; feedEl]
+      (Jstr.of_string "div")
+      [date_el; title_el; feed_el]
   in
   let _ =
     Ev.listen Ev.click
       (fun _ ->
-        let contentEl = get_element_by_id_exn "content" in
-        let contentHash = Jv.get data "content" |> Jv.to_jstr in
-        let contentUrl =
-          Jstr.append (Jstr.of_string "/elfeed/content/") contentHash
+        let content_el = get_element_by_id_exn "content" in
+        let content_hash = Jv.get data "content" |> Jv.to_jstr in
+        let content_url =
+          Jstr.append (Jstr.of_string "/elfeed/content/") content_hash
         in
         (* Set src of IFrame *)
-        El.set_at At.Name.src (Some contentUrl) contentEl ;
+        El.set_at At.Name.src (Some content_url) content_el ;
         (* Set reading mode *)
         Document.body G.document |> El.set_class (Jstr.of_string "reading") true ;
         () )
@@ -71,14 +72,14 @@ let makeEntry data =
   in
   entry
 
-let displayResults response =
+let display_results response =
   let open Fut.Result_syntax in
   let headers = Fetch.Response.headers response in
   let* data = response |> Fetch.Response.as_body |> Fetch.Body.json in
   let data_list = Jv.to_jv_list data in
-  let children = data |> Jv.to_jv_list |> List.map makeEntry in
-  let resultsEl = get_element_by_id_exn "results" in
-  El.set_children resultsEl children ;
+  let children = data |> Jv.to_jv_list |> List.map make_entry in
+  let results_el = get_element_by_id_exn "results" in
+  El.set_children results_el children ;
   let n = data_list |> List.length in
   let loaded = "loaded " ^ string_of_int n ^ " items" in
   let status =
@@ -97,7 +98,7 @@ let displayResults response =
     | None ->
         loaded ^ " (from the server)"
   in
-  setStatus status ; Fut.ok ()
+  set_status status ; Fut.ok ()
 
 let search () =
   let base = "/elfeed/search?q=" |> Jstr.of_string in
@@ -108,7 +109,7 @@ let search () =
         e |> Jv.Error.message |> Jstr.to_string
         |> fun s -> "Failed to search with error:" ^ s
       in
-      setStatus msg ; failwith msg
+      set_status msg ; failwith msg
   | Ok s ->
       let open Fut.Result_syntax in
       let url = Jstr.append base s in
@@ -117,35 +118,35 @@ let search () =
       let status = Fetch.Response.status response in
       ignore
         ( if status <> 200 then (
-            setStatus "offline or backend unavailable & no cache data found" ;
+            set_status "offline or backend unavailable & no cache data found" ;
             failwith "Server returned with status code: " ^ string_of_int status
             )
           else (
-            Fut.await (displayResults response) (fun _ -> ()) ;
+            Fut.await (display_results response) (fun _ -> ()) ;
             "" ) ) ;
       Fut.ok ()
 
 let () =
   (* Hook up search-form submit event handler *)
-  let formEl = get_element_by_id_exn "search-form" in
+  let form_el = get_element_by_id_exn "search-form" in
   let _listener =
     Ev.listen submit
       (fun e ->
         Ev.prevent_default e ;
-        setStatus "searching ..." ;
+        set_status "searching ..." ;
         Fut.await (search ()) (fun _ -> ()) )
-      (El.as_target formEl)
+      (El.as_target form_el)
   in
   (* Hook up back-btn click handler *)
-  let backBtnEl = get_element_by_id_exn "back" in
+  let back_btn_el = get_element_by_id_exn "back" in
   let _ =
     Ev.listen Ev.click
       (fun _ ->
         Document.body G.document
         |> El.set_class (Jstr.of_string "reading") false )
-      (El.as_target backBtnEl)
+      (El.as_target back_btn_el)
   in
   (* Initial load *)
-  let qEl = get_element_by_id_exn "q" in
-  El.set_at At.Name.value (Some (Jstr.of_string "@30-days-old")) qEl ;
+  let q_el = get_element_by_id_exn "q" in
+  El.set_at At.Name.value (Some (Jstr.of_string "@30-days-old")) q_el ;
   Fut.await (search ()) (fun _ -> ())
