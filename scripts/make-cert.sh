41b1df675eb40a538c070bc311e5a016a13fb65f
Puneeth Chaganti <punchagan@muse-amuse.in>
2025-12-15 14:23:37 +0530

    Include $(hostname).local in certificate domains
    Also, copy the root certificate to ssl directory for convenience to copy
    to the mobile device.

--- scripts/make-cert.sh
+++ scripts/make-cert.sh
@@ -19,11 +19,13 @@ else
 fi
 
 mkdir -p ssl/
-mkcert -key-file ssl/server.key -cert-file ssl/server.pem "$IP_ADDRESS" 127.0.0.1 localhost
-
-echo "New SSL certificates have been generated ✅"
+mkcert -key-file ssl/server.key -cert-file ssl/server.pem "$IP_ADDRESS" 127.0.0.1 localhost "$(hostname).local"
 
 CERT_PATH=$(mkcert -CAROOT)/rootCA.pem
+cp "$CERT_PATH" "ssl/rootCA.crt"
+
+echo "New SSL certificates have been generated ✅"
 echo ""
 echo "NOTE: 📋 You need to add the '${CERT_PATH}' file to your trusted certificates."
+echo "It has also been copied to ssl/rootCA.crt for your convenience."
 echo "See https://github.com/FiloSottile/mkcert?tab=readme-ov-file#mobile-devices"
