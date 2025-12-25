02aa3920f4c58898186016b414b5bfe6b2847c00
Puneeth Chaganti <punchagan@muse-amuse.in>
2025-12-25 19:14:22 +0530

    Simple fetch handler with couple of cache strategies
    Use cache_first_with_refresh for shell contents. We use existing shell,
    but refresh the cache for the next request.
    
    Use the cache_first strategy for entry content. Content at a particular
    hash would never change. New content would have a new hash. So, if we
    find a content hash in the cache, we always use it!

--- web/sw.ml
+++ web/sw.ml
@@ -17,6 +17,42 @@ module Config = struct
     |> List.map Jstr.v
 end
 
+(** Helpers to return content by strategy  *)
+module Fetch_strategy = struct
+  let _network_request_and_cache ~cache request =
+    let open Fut.Result_syntax in
+    let* response = Fetch.request request in
+    let clone = Fetch.Response.of_response response in
+    let status = Fetch.Response.status response in
+    (* Cache the request only on success *)
+    if status >= 200 && status < 300 then
+      Fetch.Cache.put cache request clone |> ignore ;
+    Fut.ok response
+
+  let cache_first request cache_name =
+    let open Fut.Result_syntax in
+    let storage = Fetch.caches () in
+    let* cache = Cache_storage.open' storage cache_name in
+    let* cached_response = Fetch.Cache.match' cache request in
+    match cached_response with
+    | Some response ->
+        Fut.ok response
+    | None ->
+        _network_request_and_cache ~cache request
+
+  let cache_first_with_refresh request cache_name =
+    let open Fut.Result_syntax in
+    let storage = Fetch.caches () in
+    let* cache = Cache_storage.open' storage cache_name in
+    let* cached_response = Fetch.Cache.match' cache request in
+    match cached_response with
+    | Some response ->
+        let _ = _network_request_and_cache ~cache request in
+        Fut.ok response
+    | None ->
+        _network_request_and_cache ~cache request
+end
+
 let on_install e =
   let request_shell =
     let open Fut.Result_syntax in
@@ -48,7 +84,35 @@ let on_activate e =
   Console.(log [Jv.of_string "Service worker activated."]) ;
   ()
 
+let on_fetch e =
+  let e = Ev.as_type e in
+  let request = Fetch.Ev.request e in
+  let url = Fetch.Request.url request in
+  let _ =
+    let open Result.Syntax in
+    let* uri = Uri.of_jstr url in
+    let path = Uri.path uri in
+    ( match path with
+    (* We update the SHELL on install, so use what's in the cache! But,
+         refresh for next time. *)
+    | s when List.mem s Config.shell ->
+        let response =
+          Fetch_strategy.cache_first_with_refresh request Config.c_shell
+        in
+        Fetch.Ev.respond_with e response
+    (* Content at a hash would never change *)
+    | s when Jstr.starts_with ~prefix:(Jstr.of_string "/elfeed/content") path ->
+        let response = Fetch_strategy.cache_first request Config.c_content in
+        Fetch.Ev.respond_with e response
+    | _ ->
+        Console.log [Jv.of_string "SW fetch"; Jv.of_jstr path] ;
+        () ) ;
+    Result.ok ()
+  in
+  ()
+
 let () =
   let self = Ev.target_of_jv Jv.global in
-  Ev.listen Ev.install on_install self |> ignore ;
-  Ev.listen Ev.activate on_activate self |> ignore
+  ignore (Ev.listen Ev.install on_install self) ;
+  ignore (Ev.listen Ev.activate on_activate self) ;
+  ignore (Ev.listen Fetch.Ev.fetch on_fetch self)
