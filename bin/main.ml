048541d3ff37b5a07df741991c4e36fe863461c0
Puneeth Chaganti <punchagan@muse-amuse.in>
2026-01-08 10:45:57 +0530

    Make turning off authentication an explicit user action
    Closes #29

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 module Proxy = Elfeed_offline.Proxy
 
-let () =
+let main ~no_auth =
   let upstream = Uri.of_string "http://127.0.0.1:8080" in
   let certificate_file = "ssl/server.pem" in
   let key_file = "ssl/server.key" in
@@ -8,18 +8,20 @@ let () =
     failwith
       "SSL certificate or key file not found. Please generate them by running \
        the make-cert.sh script." ;
-  let username = Sys.getenv_opt "ELFEED_USERNAME" in
-  let password = Sys.getenv_opt "ELFEED_PASSWORD" in
   let auth =
-    match (username, password) with
-    | Some username, Some password ->
-        Proxy.make_basic_auth_middleware ~username ~password ()
-    | Some _, _ | _, Some _ ->
-        failwith
-          "Both ELFEED_USERNAME and ELFEED_PASSWORD must be set for \
-           authentication."
-    | _ ->
-        fun handler req -> handler req
+    if no_auth then fun handler req -> handler req
+    else
+      let username = Sys.getenv_opt "ELFEED_USERNAME" in
+      let password = Sys.getenv_opt "ELFEED_PASSWORD" in
+      match (username, password) with
+      | Some username, Some password ->
+          Proxy.make_basic_auth_middleware ~username ~password ()
+      | _ ->
+          Printf.eprintf
+            "Authentication is enabled but ELFEED_USERNAME and/or \
+             ELFEED_PASSWORD environment variables are not set. Either set \
+             both variables or run with --no-auth to disable authentication.\n" ;
+          exit 1
   in
   let open Dream in
   let routes =
@@ -33,3 +35,21 @@ let () =
   in
   run ~interface:"0.0.0.0" ~port:9000 ~certificate_file ~key_file ~tls:true
   @@ logger @@ auth @@ router routes
+
+let () =
+  let open Cmdliner in
+  let open Term.Syntax in
+  let no_auth =
+    let doc = "Disable authentication (allow unauthenticated access)" in
+    Arg.(value & flag & info ["no-auth"] ~doc)
+  in
+  let term =
+    let+ no_auth = no_auth in
+    main ~no_auth
+  in
+  let cmd =
+    let doc = "Elfeed offline proxy server" in
+    let info = Cmd.info "elfeed-offline" ~doc in
+    Cmd.v info term
+  in
+  exit (Cmd.eval cmd)
